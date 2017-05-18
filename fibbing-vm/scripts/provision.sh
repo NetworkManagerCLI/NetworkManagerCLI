4a43638ca620361c59b346c2f4b35b12a42b3ea7
vdbulcke <cedric.vandenbulcke@student.uclouvain.be>
2017-05-18 11:54:46 +0200

    install Fork of Fibbing

--- fibbing-vm/scripts/provision.sh
+++ fibbing-vm/scripts/provision.sh
@@ -15,6 +15,17 @@ clone() {
     fi
 }
 
+clone_fork() {
+    progress "Cloning $2"
+    if [[ -d "$1" ]]; then
+        cd ${1}
+        git pull
+        cd ..
+    else
+        git clone --recursive "https://github.com/NetworkManagerCLI/$1"
+    fi
+}
+
 progress "Setting up root ssh login"
 mkdir -p /root/.ssh
 cat /home/vagrant/.ssh/authorized_keys >> /root/.ssh/authorized_keys
@@ -47,7 +58,7 @@ progress "Installing Mininet"
 git clone https://github.com/mininet/mininet.git
 ./mininet/util/install.sh -n
 
-clone fibbingnode "the fibbing sources"
+clone_fork fibbingnode "the fibbing sources"
 clone labs "the fibbing labs"
 
 progress "Installing the fibbing controller"
